7aa83d3fdfcad6301ccd81fba3b26e1609efa74d
Bondok6 <kirolousbondok6@gmail.com>
2022-01-25 22:20:24 +0200

    Add modules folder
    Create addItem, renderData, formData functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import formData from "./modules/form";
+import render from "./modules/renderData";
 import "./style.css";
+
+document.querySelector("#form").addEventListener("submit", formData);
+// prettier-ignore
+document.querySelector("#refresh-btn").addEventListener("click", () => window.location.reload());
+
+render();
---
+++ src/modules/addItem.js
@@ -0,0 +1,11 @@
+import renderItem from "./renderItem";
+import Data from "./data.js";
+
+const addItem = (name, score) => {
+  const newItem = new Data(name, score);
+  Data.scores.push(newItem);
+  Data.setStorage();
+  renderItem(name, score);
+};
+
+export default addItem;
